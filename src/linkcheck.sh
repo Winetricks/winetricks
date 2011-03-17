7b696da54db418d89cc472367628a71180845e2b
daniel.r.kegel <daniel.r.kegel@98bf863f-ac88-fb5c-4855-c94565d26d3c>
2011-03-17 05:04:21 +0000

    First draft of link checker.  Hangs too much still, --connect-timeout doesn't seem to do anything.
    git-svn-id: https://winetricks.googlecode.com/svn/trunk@307 98bf863f-ac88-fb5c-4855-c94565d26d3c

---
+++ src/linkcheck.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+for url in ` grep '^ *w_download ' winetricks | sort | grep http | sed 's/^ *//' | awk '{print $2}' | tr -d '"'"'" `
+do
+    echo -n "$url "
+    curl --connect-timeout 10 --retry 6 -s -S -I "$url" > tmp
+    cat tmp >> tmp.all
+    if test "`grep HTTP < tmp | grep " 200 " `" = ""
+    then
+        echo "$url bad"
+    fi
+    cat tmp | egrep 'Last-Modified:|Content-Length:|ETag:' | tr '\012' ' ' | tr -d '\015' | sed 's/ Connection:.*//'
+    echo ""
+done > linkcheck.out
+
+diff -u linkcheck.ref linkcheck.out
+
