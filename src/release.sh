9a252d6d6b71fea1467316f9a84b54f256cd4e9d
daniel.r.kegel@gmail.com <daniel.r.kegel@gmail.com@98bf863f-ac88-fb5c-4855-c94565d26d3c>
2011-03-29 13:05:11 +0000

    release.sh: tarballs, baby
    git-svn-id: https://winetricks.googlecode.com/svn/trunk@392 98bf863f-ac88-fb5c-4855-c94565d26d3c

--- src/release.sh
+++ src/release.sh
@@ -4,9 +4,7 @@ set -e
 version=`grep '^WINETRICKS_VERSION' < winetricks | sed 's/.*=//'`
 echo Pushing version $version
 
-cp winetricks winetricks-$version
-gzip < winetricks-$version > winetricks-$version.gz
-sha1sum winetricks-$version > winetricks-$version.sha1.txt
-sha1sum winetricks-$version.gz > winetricks-$version.gz.sha1.txt
-scp winetricks-$version winetricks-$version.gz winetricks-$version.sha1.txt winetricks-$version.gz.sha1.txt kegel.com:public_html/winetricks/download/releases/
+tar -czvf winetricks-$version.tgz winetricks
+sha1sum winetricks-$version.tgz > winetricks-$version.tgz.sha1.txt
+scp winetricks-$version.tgz winetricks-$version.tgz.sha1.txt kegel.com:public_html/winetricks/download/releases/
 scp winetricks kegel.com:public_html/kegel/wine/
