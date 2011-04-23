03b5c6b4f50286659af25586d3b003a642c8d5a7
daniel.r.kegel <daniel.r.kegel@98bf863f-ac88-fb5c-4855-c94565d26d3c>
2011-04-23 13:45:51 +0000

    Add man page and COPYING file to tarball.
    git-svn-id: https://winetricks.googlecode.com/svn/trunk@502 98bf863f-ac88-fb5c-4855-c94565d26d3c

--- src/release.sh
+++ src/release.sh
@@ -4,7 +4,9 @@ set -e
 version=`grep '^WINETRICKS_VERSION' < winetricks | sed 's/.*=//'`
 echo Pushing version $version
 
-tar -czvf winetricks-$version.tgz winetricks
+# FIXME: Should adjust version and date in man page?
+# FIXME: making the tarball should probably be the responsibility of 'make tarball'
+tar -czvf winetricks-$version.tgz winetricks winetricks.1 COPYING
 sha1sum winetricks-$version.tgz > winetricks-$version.tgz.sha1.txt
 scp winetricks-$version.tgz winetricks-$version.tgz.sha1.txt kegel.com:public_html/winetricks/download/releases/
 scp winetricks kegel.com:public_html/winetricks/
