3d38c6b533ebe9655bd460f7d6598a42db4d6902
daniel.r.kegel@gmail.com <daniel.r.kegel@gmail.com@98bf863f-ac88-fb5c-4855-c94565d26d3c>
2011-06-20 03:56:45 +0000

    linkcheck.sh: refactored, parallelized, broke into data gathering & reporting modes
    git-svn-id: https://winetricks.googlecode.com/svn/trunk@629 98bf863f-ac88-fb5c-4855-c94565d26d3c

--- src/linkcheck.sh
+++ src/linkcheck.sh
@@ -1,20 +1,87 @@
 #!/bin/sh
-set -x
-for url in ` grep '^ *w_download ' winetricks | sort | grep http | sed 's/^ *//' | awk '{print $2}' | tr -d '"'"'" `
-do
-    echo -n "$url "
-    curl --connect-timeout 10 --retry 6 -s -S -I "$url" > tmp
-    cat tmp >> tmp.all
-    if test "`grep HTTP < tmp | egrep " 200 | 302 " `" = ""
+datadir="links.d"
+
+# Extract list of URLs from winetricks
+extract_all() {
+    for url in ` grep '^ *w_download ' winetricks | sort | grep http | sed 's/^ *//' | awk '{print $2}' | tr -d '"'"'" `
+    do
+        urlkey=`echo $url | tr / _`
+        echo "$url" > "$datadir"/"$urlkey.url"
+    done
+}
+
+# Show results for a given url
+# Input: .url file
+# Output: line with OK or BAD followed by URL, 
+# optionally followed by lines with more detail
+show_one() {
+    urlfile=$1
+    base=${urlfile%.url}
+    url="`cat $urlfile`"
+    if grep "HTTP.*404" "$base.log"
     then
-        echo "----------"
-        echo "$url bad"
-        cat tmp
+        echo "BAD $url"
+        cat "$base.log"
         echo ""
     fi
-    cat tmp | egrep 'Last-Modified:|Content-Length:|ETag:' | tr '\012' ' ' | tr -d '\015' | sed 's/ Connection:.*//'
-    echo ""
-done > linkcheck.out
+}
 
-diff -u linkcheck.ref linkcheck.out
+# Show full report on most recent crawl
+show_all() {
+    for urlfile in "$datadir"/*.url
+    do
+        show_one "$urlfile"
+    done
+}
 
+# Save info about the given url to a file
+# Input: .url file
+# Output:
+# .log gets the full info
+# .dat gets a summary
+# Calls show_one to print results out as they come in
+crawl_one() {
+    urlfile=$1
+    base=${urlfile%.url}
+    url="`cat $urlfile`"
+
+    curl --connect-timeout 10 --retry 6 -s -S -I "$url" 2>&1 | 
+       tr -d '\015' |
+       grep . |
+       sort > "$base.log"
+    # more diff-able?
+    #cat "$base.log" | 
+    #  egrep 'HTTP|Last-Modified:|Content-Length:|ETag:' |
+    #  tr '\012' ' ' | 
+    #  sed 's/ Connection:.*//' > "$datadir"/"$urlkey.dat"
+    #echo "" >> "$base.dat"
+    show_one "$urlfile"
+}
+
+# Fetch all info
+# Do fetches in background so slow servers don't hang us
+# Print quick feedback as results come in
+crawl_all() {
+    for urlfile in "$datadir"/*.url
+    do
+        url="`cat $urlfile`"
+        echo "Crawling $url"
+        crawl_one "$urlfile" &
+        sleep 1
+    done 
+    # Wait for fetches to finish
+    wait
+}
+
+mkdir -p "$datadir"
+
+case "$1" in
+crawl) 
+    extract_all
+    crawl_all
+    show_all
+    ;;
+report)
+    show_all
+    ;;
+esac
