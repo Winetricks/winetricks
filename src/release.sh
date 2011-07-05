2472a1b072aa49bda1833c8ebad51a04ed03a0d9
daniel.r.kegel <daniel.r.kegel@98bf863f-ac88-fb5c-4855-c94565d26d3c>
2011-07-05 00:50:11 +0000

    release.sh: make winetricks show up first in sha1 file
    git-svn-id: https://winetricks.googlecode.com/svn/trunk@669 98bf863f-ac88-fb5c-4855-c94565d26d3c

--- src/release.sh
+++ src/release.sh
@@ -7,6 +7,6 @@ echo Pushing version $version
 # FIXME: Should adjust version and date in man page?
 # FIXME: making the tarball should probably be the responsibility of 'make tarball'
 tar -czvf winetricks-$version.tgz winetricks winetricks.1 COPYING
-sha1sum winetricks-$version.tgz winetricks > winetricks-$version.tgz.sha1.txt
+sha1sum winetricks winetricks-$version.tgz > winetricks-$version.tgz.sha1.txt
 scp winetricks-$version.tgz winetricks-$version.tgz.sha1.txt kegel.com:public_html/winetricks/download/releases/
 scp winetricks kegel.com:public_html/winetricks/
