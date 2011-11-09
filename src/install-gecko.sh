29ee3274f418ea44bfc5f5733f7a68d0389b5082
daniel.r.kegel <daniel.r.kegel@98bf863f-ac88-fb5c-4855-c94565d26d3c>
2011-11-09 18:47:38 +0000

    merge gecko-1.4 support from winezeug
    git-svn-id: https://winetricks.googlecode.com/svn/trunk@713 98bf863f-ac88-fb5c-4855-c94565d26d3c

--- src/install-gecko.sh
+++ src/install-gecko.sh
@@ -23,6 +23,14 @@ install_gecko()
         esac
         GECKO_SUFFIX=.msi
         ;;
+    gecko-1.4)
+        GECKO_VERSION=1.4
+        case $myarch in
+        x86)   GECKO_SHA1SUM=c30aa99621e98336eb4b7e2074118b8af8ea2ad5 ;;
+        x86_64) GECKO_SHA1SUM=bf0aaf56a8cf9abd75be02b56b05e5c4e9a4df93 ;;
+        esac
+        GECKO_SUFFIX=.msi
+        ;;
     *)
         GECKO_VERSION=1.1.0
         GECKO_SHA1SUM=1b6c637207b6f032ae8a52841db9659433482714
@@ -43,12 +51,13 @@ install_gecko()
         fi
 
         sudo mkdir -p /usr/share/wine/gecko
-        sudo cp wine_gecko-$GECKO_VERSION-$myarch$GECKO_SUFFIX /usr/share/wine/gecko/
+        sudo mv wine_gecko-$GECKO_VERSION-$myarch$GECKO_SUFFIX /usr/share/wine/gecko/
     fi
 }
 
 # Install gecko for stable wine and the current dev branch
 myarch=x86
+install_gecko gecko-1.4
 install_gecko gecko-1.3
 install_gecko wine-1.2
 install_gecko wine-1.3.3
