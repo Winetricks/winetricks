474d4bbc8e8e8fdb9179ee711cd26c08da50dd0c
daniel.r.kegel <daniel.r.kegel@98bf863f-ac88-fb5c-4855-c94565d26d3c>
2013-06-18 04:31:26 +0000

    release.sh now uses Makefile to generate tarball.  Must run from top level now.
    git-svn-id: https://winetricks.googlecode.com/svn/trunk@994 98bf863f-ac88-fb5c-4855-c94565d26d3c

--- src/release.sh
+++ src/release.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 set -x
 set -e
-version=`grep '^WINETRICKS_VERSION' < winetricks | sed 's/.*=//'`
+version=`grep '^WINETRICKS_VERSION' < src/winetricks | sed 's/.*=//'`
 echo Pushing version $version
 
 # FIXME: Should adjust version and date in man page?
-# FIXME: making the tarball should probably be the responsibility of 'make tarball'
-tar -czvf winetricks-$version.tgz winetricks winetricks.1 COPYING
-sha1sum winetricks winetricks-$version.tgz > winetricks-$version.tgz.sha1.txt
-scp winetricks-$version.tgz winetricks-$version.tgz.sha1.txt kegel.com:public_html/winetricks/download/releases/
-scp winetricks kegel.com:public_html/winetricks/
+make dist
+sha1sum src/winetricks winetricks-$version.tar.gz | sed 's,src/,,' > winetricks-$version.tar.gz.sha1.txt
+scp winetricks-$version.tar.gz winetricks-$version.tar.gz.sha1.txt kegel.com:public_html/winetricks/download/releases/
+scp src/winetricks kegel.com:public_html/winetricks/
