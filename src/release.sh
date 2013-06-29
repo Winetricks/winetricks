1cd698cec0f7c4e5fcd4744bc22db47bcedd351d
daniel.r.kegel <daniel.r.kegel@98bf863f-ac88-fb5c-4855-c94565d26d3c>
2013-06-29 17:46:19 +0000

    release.sh: add copyright header
    git-svn-id: https://winetricks.googlecode.com/svn/trunk@1023 98bf863f-ac88-fb5c-4855-c94565d26d3c

--- src/release.sh
+++ src/release.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+# Trivial release helper for winetricks.
+#
+# Copyright (C) 2011,2012,2013 Dan Kegel.
+#
+# This software comes with ABSOLUTELY NO WARRANTY.
+#
+# This is free software, placed under the terms of the
+# GNU Lesser Public License version 2.1, as published by the Free Software
+# Foundation. Please see the file COPYING for details.
 set -x
 set -e
 version=`grep '^WINETRICKS_VERSION' < src/winetricks | sed 's/.*=//'`
