6c766e119e005a4ee0169f6b856e32164464c0e8
Austin English <austinenglish@gmail.com>
2019-11-26 04:42:53 -0600

    misc: put 'do' on the same line as 'do'/'while'

--- src/uniquefiles.sh
+++ src/uniquefiles.sh
@@ -20,7 +20,8 @@ fi
 
 # Generate list of all filenames (except those which look ephemeral)
 rm -f /tmp/allfiles.txt
-for dir ; do
+
+for dir; do
     (
         cd "$dir/drive_c"
         # FIXME: don't assume there are no ='s in filenames, e.g. rewrite in perl
@@ -33,7 +34,7 @@ done
 sort < /tmp/allfiles.txt | uniq -c | awk '$1 == 1 {print $2}' > /tmp/uniqfiles.txt
 
 # Associate them with the verb they came from
-for dir ; do
+for dir; do
     (
         cd "$dir"
         # Undo the space-to-= transformation, too
