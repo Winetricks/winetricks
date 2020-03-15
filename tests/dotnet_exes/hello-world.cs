665b120b74b42bba01caf7283b494ce320d718a7
Austin English <austinenglish@gmail.com>
2020-03-15 01:36:15 -0500

    better hello world, maybe

--- tests/dotnet_exes/hello-world.cs
+++ tests/dotnet_exes/hello-world.cs
@@ -1,11 +1,19 @@
 // Hello world
 
+// this shows a way to print the version:
+// 
+//     var assembly = Assembly.GetAssembly(typeof(Program));
+//         Console.WriteLine(assembly.ImageRuntimeVersion);
+//             // prints "v2.0.50727"
+//
+// https://stackoverflow.com/questions/36463047/how-do-i-compile-for-net-2-0-with-c-sharp-6-0
+
 using System;
 
-namespace hello {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-        }
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Hello world");
     }
 }
