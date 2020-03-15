45029657c1cb9b10d81c0cbc254e2e63e69e13c1
Austin English <austinenglish@gmail.com>
2020-03-15 02:44:29 -0500

    stash

--- tests/dotnet_exes/build.sh
+++ tests/dotnet_exes/build.sh
@@ -16,25 +16,21 @@ if ! command -v mcs; then
     exit 1
 fi
 
-# FIXME 7/7.1/7.2 may be valid as well?
-for version in ISO-2 3 4 5 6 experimental; do
+for version in ISO-1 ISO-2 3 4 5 6 experimental; do
     # C# version to .Net version info can be found at:
     # https://www.guru99.com/c-sharp-dot-net-version-history.html
 
     case "$version" in
-    ISO-2) dn_ver="2.0"; extra_args="-r:/usr/lib/mono/3.5-api/mscorlib.dll";;
-    3) dn_ver="3.0"; extra_args="";;
-    4) dn_ver="4.0"; extra_args="";;
-    5) dn_ver="4.5"; extra_args="";;
-    6) dn_ver="4.6"; extra_args="";;
-
-    # 7
-    # 7.1
-    # 7.2
+    ISO-1) dn_ver="1.0";;
+    ISO-2) dn_ver="2.0";;
+    3) dn_ver="3.0";;
+    4) dn_ver="4.0";;
+    5) dn_ver="4.5";;
+    6) dn_ver="4.6";;
 
     # FIXME: this will change as mono supports newer versions, ideally should find
     # a more stable solution.
-    experimental) dn_ver="4.7-experimental"; extra_args="";;
+    experimental) dn_ver="4.7-experimental";;
 
     *) echo "unknown version"; exit 1;;
     esac
@@ -44,6 +40,6 @@ for version in ISO-2 3 4 5 6 experimental; do
         output="dotnet-hello-world-${dn_ver}-${arch}.exe"
         echo "Building ${output} for C# version ${version} / arch=${arch}"
 
-        mcs -nostdlib "$extra_args" "-langversion:${version}" "-out:${output}" "-platform:${arch}" hello-world.cs
+        mcs "-langversion:${version}" "-out:${output}" "-platform:${arch}" hello-world.cs
     done
 done
--- tests/dotnet_exes/hello-world.cs
+++ tests/dotnet_exes/hello-world.cs
@@ -1,19 +1,11 @@
 // Hello world
 
-// this shows a way to print the version:
-// 
-//     var assembly = Assembly.GetAssembly(typeof(Program));
-//         Console.WriteLine(assembly.ImageRuntimeVersion);
-//             // prints "v2.0.50727"
-//
-// https://stackoverflow.com/questions/36463047/how-do-i-compile-for-net-2-0-with-c-sharp-6-0
-
 using System;
 
-public class HelloWorld
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine ("Hello world");
+namespace hello {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
     }
 }
